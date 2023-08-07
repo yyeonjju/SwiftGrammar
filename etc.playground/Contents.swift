13969c8ac47fd0dcf7cabd9f74d486a0038a407e
yyeonjju <llilyha123@gmail.com>
2023-08-07 17:41:21 +0900

    연산자

---
+++ etc.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//플레이그라운드 마크업
+//https://oeepak.tistory.com/227
+// Editor-Show Raw Markup
+//단축키 option+M
+
+
+/*:
+## 2) 기본 연산자
+ */
+
+//Mark:- 연산자
+//단항 연산자 (Unary Operator)
+//이항 연산자 (Binary Operator)
+//삼항 연산자 (Ternary Operator)
+
+
+//할당 연산자 (Assignment Operator)
+//산술 연산자 (Arithmatic Operator)
+
+
+//프로그래밍에서는 나누기 사용은 항상 조심
+// 나누기 (/) 정수끼리 연산에서는 몫이 나옴
+var a = 4
+var b = 5
+
+
+var newResult: Double
+
+//newResult = a / b
+print(a / b)
+newResult = Double(a / b)
+
+newResult = Double(a) / Double(b)
+
+//복합 할당 연산자 (Compound Assignment Operator)
+//+= -= ...
+
+//비교 연산자 (Comparison Operator)
+
+//논리 연산자 (Logical Operator)
+//! && ||
+
+//접근 연산자 (.)
+
+
+
+
+
